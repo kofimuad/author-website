73b36413fa4bdd31d477911691589617c4d7477b
kofimuad <iamkofiagyei@gmail.com>
2025-12-25 00:23:50 +0000

    preparing for deployment

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import { getToken } from './auth'
 
-const API_BASE_URL = 'http://localhost:5000/api'
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api'
 
 // Helper function for API calls
 const apiCall = async (endpoint, method = 'GET', data = null) => {
